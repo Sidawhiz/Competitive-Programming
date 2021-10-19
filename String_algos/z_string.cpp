8f91de5a2b7deeb9b851bab99b0ec5b42531451f
Siddhesh Kalekar <66837737+Sidawhiz@users.noreply.github.com>
2021-10-19 13:36:22 +0530

    Update z_string.cpp

--- String_algos/z_string.cpp
+++ String_algos/z_string.cpp
@@ -1,43 +1,52 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define ll long long int
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+typedef vector<pi> vpi;
+typedef vector<vi> vvi;
+
 #define pb push_back
 #define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define all(x) x.begin(), x.end()
+#define mod 1000000007
 
-string s;
-vector<int> z;
 
-void z_function(){
-    int L = 0, R = 0;
-    int n = s.size();
-    for(int i = 1; i<n ; i++){
-        if(i>R){
-            L = i;
-            R = i;
-            while(R<n && s[R-L]==s[R]){
-                R++;
+vi z_function(string s){
+    int l = 0, r=0 , n =s.size();
+    vi z(n,0);
+    for(int i = 1 ; i<n ; i++){
+        if(i>r){
+            l = r = i;
+            while(r<n && s[r-l]==s[r]){
+                r++;
             }
-            z[i] = R-L;
-            R--;
+            z[i] = r-l;
+            r--;
         }
         else{
-            int dist_from_start = i-L;
-            if(i + z[dist_from_start] <= R){
-                z[i] = z[dist_from_start];
+            int dist = i-l;
+            if(i + z[dist] <= r){
+                z[i] = z[dist];
             }
             else{
-                L = i;
-                while(R<n && s[R-L]==s[R]){
-                    R++;
+                l = i; 
+                dist = 0;
+                while(r<n && s[r-l]==s[r]){
+                    r++;
                 }
-                z[i] = R-L;
-                R--;
+                z[i] = r-l;
+                r--;
             }
         }
     }
-
+    return z;
 }
+
 
 int main(){
     fast;
@@ -48,11 +57,11 @@ int main(){
     freopen("output.txt", "w", stdout);
     #endif
 
+    string s;
     cin >> s;
     int n = s.size();
-    vector<int> zz(n,0);
-    z = zz;
-    z_function();
+    vector<int> z;
+    z = z_function(s);
     for(int i = 0; i<n; i++){
         cout<<z[i]<<" ";
     }
